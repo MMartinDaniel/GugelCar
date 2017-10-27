79ccd23845a59d0df896540232c1cf919099ac56
Rubén Céspedes Heredia <rubenn569@gmail.com>
2017-10-27 18:43:36 +0200

    Add files via upload
    Mejora. Solo se recorren las ultimas 50 pisadas. Se empieza por el final

---
+++ Helloworld.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+import es.upv.dsic.gti_ia.core.ACLMessage;
+import es.upv.dsic.gti_ia.core.AgentID;
+import es.upv.dsic.gti_ia.core.AgentsConnection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Dani
+ */
+public class Helloworld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Agente a;
+     //   AgentsConnection.connect("localhost",5672,"test","guest","guest",false);  
+       AgentsConnection.connect("isg2.ugr.es",6000,"Bellatrix","Escorpion","Russo",false);
+        try {
+
+            a = new Agente(new AgentID("agentep13"));
+            a .start();
+     
+        } catch (Exception ex) {
+            System.out.println("Error al crear el agente ");
+        }
+
+      
+    }
+    
+}
